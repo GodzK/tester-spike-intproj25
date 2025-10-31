8c4582af3dc291bc79d240821b6a0e363dd6dc83
Phakaphol Dherachaisuphakij <godzk25@gmail.com>
2025-10-31 21:32:35 +0700

    add a file

--- db_init/init.sql
+++ db_init/init.sql
@@ -1,15 +1,27 @@
 CREATE DATABASE IF NOT EXISTS mydb;
+CHARACTER SET utf8mb4
+COLLATE utf8mb4_unicode_ci;
 USE mydb;
+CREATE TABLE IF NOT EXISTS study_plan (
+  `id` INT AUTO_INCREMENT PRIMARY KEY,
+  `plan_code` CHAR(2) NOT NULL UNIQUE,
+  `name_eng` VARCHAR(60) NOT NULL,
+  `name_th` VARCHAR(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
-CREATE TABLE IF NOT EXISTS students (
-  id INT AUTO_INCREMENT PRIMARY KEY,
-  name VARCHAR(100),
-  email VARCHAR(100)
-);
+CREATE TABLE IF NOT EXISTS students_plans (
+  `student_id` VARCHAR(11) PRIMARY KEY,
+  `plan_id` INT NOT NULL,
+  `created_at` DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `updated_at` DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  FOREIGN KEY (plan_id) REFERENCES study_plan(id)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
-INSERT INTO students (name, email) VALUES
-('Alice', 'alice@email.com'),
-('Bob', 'bob@email.com');
+INSERT INTO study_plan (plan_code, name_eng, name_th) VALUES
+('FE', 'Frontend Developer', 'นักพัฒนาฟรอนเอนด์'),
+('BE', 'Backend Developer', 'นักพัฒนาแบ็กเอนด์'),
+('FS', 'Full-Stack Developer', 'นักพัฒนาฟูลสแต็ก'),
+('AI', 'AI Engineer', 'วิศวกรปัญญาประดิษฐ์'),
+('UX', 'UI/UX Designer', 'นักออกแบบ UI/UX'),
+('DA', 'Data Analyst', 'นักวิเคราะห์ข้อมูล');
 
-
--- docker-compose down -v เพื่อลบ volume เเละ compose up เพื่อ สร้างdbใหม่
